26d6e4b02d050198fa8d5f49d0653929e447980e
khhan <khhan@umich,edu>
2023-02-13 00:51:23 -0500

    Now really fixed merge conflicts.

--- app/src/main/java/com/example/filtervision2/MainActivity.java
+++ app/src/main/java/com/example/filtervision2/MainActivity.java
@@ -7,15 +7,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.graphics.PorterDuff;
+import android.widget.RadioButton;
 import android.widget.SeekBar;
 
 import androidx.appcompat.widget.SwitchCompat;
+import androidx.appcompat.widget.AppCompatSeekBar;
 
 public class MainActivity extends AppCompatActivity {
-    SwitchCompat red;
-    SwitchCompat def;
-    SwitchCompat cust1;
-    SwitchCompat cust2;
+    RadioButton def;
+    RadioButton red;
+    RadioButton green;
+    RadioButton blue;
     SeekBar red_slider;
     SeekBar green_slider;
     SeekBar blue_slider;
@@ -24,22 +26,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        red = findViewById(R.id.red_switch);
-        def = findViewById(R.id.default_switch);
-        cust1 = findViewById(R.id.custom_switch1);
-        cust2 = findViewById(R.id.custom_switch2);
+        def = findViewById(R.id.default_button);
+        red = findViewById(R.id.red_button);
+        green = findViewById(R.id.green_button);
+        blue = findViewById(R.id.blue_button);
+
         red_slider = findViewById(R.id.red_slider);
         green_slider = findViewById(R.id.green_slider);
         blue_slider = findViewById(R.id.blue_slider);
+
         red_slider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             int progressChangedValue = 0;
 
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 progressChangedValue = progress;
                 def.setChecked(false);
-                cust1.setChecked(false);
                 red.setChecked(false);
-                cust2.setChecked(false);
+                green.setChecked(false);
+                blue.setChecked(false);
             }
 
             public void onStartTrackingTouch(SeekBar seekBar) {
@@ -56,9 +60,9 @@ public class MainActivity extends AppCompatActivity {
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 progressChangedValue = progress;
                 def.setChecked(false);
-                cust1.setChecked(false);
                 red.setChecked(false);
-                cust2.setChecked(false);
+                green.setChecked(false);
+                blue.setChecked(false);
             }
 
             public void onStartTrackingTouch(SeekBar seekBar) {
@@ -75,9 +79,9 @@ public class MainActivity extends AppCompatActivity {
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 progressChangedValue = progress;
                 def.setChecked(false);
-                cust1.setChecked(false);
                 red.setChecked(false);
-                cust2.setChecked(false);
+                green.setChecked(false);
+                blue.setChecked(false);
             }
 
             public void onStartTrackingTouch(SeekBar seekBar) {
@@ -88,71 +92,36 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
     }
 
-    public void selectDefault(View view) {
-        red.setChecked(false);
-        cust1.setChecked(false);
-        cust2.setChecked(false);
+    public void onClick(View view) {
         red_slider.setProgress(0);
         green_slider.setProgress(0);
         blue_slider.setProgress(0);
-    }
-
-    public void selectRed(View view) {
-        def.setChecked(false);
-        cust1.setChecked(false);
-        cust2.setChecked(false);
-        red_slider.setProgress(0);
-        green_slider.setProgress(0);
-        blue_slider.setProgress(0);
-    }
-    public void selectCustom1(View view) {
-        red.setChecked(false);
-        def.setChecked(false);
-        cust2.setChecked(false);
-        red_slider.setProgress(0);
-        green_slider.setProgress(0);
-        blue_slider.setProgress(0);
-    }
-
-    public void selectCustom2(View view) {
-        def.setChecked(false);
-        cust1.setChecked(false);
-        red.setChecked(false);
-        red_slider.setProgress(0);
-        green_slider.setProgress(0);
-        blue_slider.setProgress(0);
+        // more functionality, if needed
     }
 
     public void applyFilter(View view) {
         ImageView mountains = findViewById(R.id.image);
-        if (red.isChecked()) {
+        if (def.isChecked()) {
+            mountains.getDrawable().clearColorFilter();
+        } else if (red.isChecked()) {
             mountains.getDrawable().setColorFilter(0xffff7276, PorterDuff.Mode.MULTIPLY);
-        } else if (def.isChecked()) {
-            mountains.getDrawable().clearColorFilter();
-        } else if (cust1.isChecked()) {
+        } else if (green.isChecked()) {
             mountains.getDrawable().setColorFilter(0xFF3700B3, PorterDuff.Mode.MULTIPLY);
-        } else if (cust2.isChecked()) {
+        } else if (blue.isChecked()) {
             mountains.getDrawable().setColorFilter(0xFF018786, PorterDuff.Mode.MULTIPLY);
-        } else if (red_slider.getProgress() == 0 && green_slider.getProgress() == 0 && blue_slider.getProgress() == 0){
+        } else if (red_slider.getProgress() == 0 && green_slider.getProgress() == 0 && blue_slider.getProgress() == 0) {
             mountains.getDrawable().clearColorFilter();
-        } else {
+        }  else {
             mountains.getDrawable().setColorFilter(Color.rgb(red_slider.getProgress(), green_slider.getProgress(), blue_slider.getProgress()), PorterDuff.Mode.MULTIPLY);
         }
     }
-    public void selectImage1(View view) {
-        SwitchCompat im2 = findViewById(R.id.select_image2);
-        im2.setChecked(false);
-    }
 
-    public void selectImage2(View view) {
-        SwitchCompat im1 = findViewById(R.id.select_image1);
-        im1.setChecked(false);
-    }
     public void applyImage(View view) {
-        SwitchCompat im1 = findViewById(R.id.select_image1);
-        SwitchCompat im2 = findViewById(R.id.select_image2);
+        RadioButton im1 = findViewById(R.id.select_image1);
+        RadioButton im2 = findViewById(R.id.select_image2);
         ImageView image = findViewById(R.id.image);
         if (im1.isChecked()) {
             image.setImageResource(R.drawable.palm_tree);
