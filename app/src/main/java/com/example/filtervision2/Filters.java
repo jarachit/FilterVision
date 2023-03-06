b1a2000ca7b867c42c0baf047ae167e1340b5e6b
Matthew Doucette <mdouce@umich.edu>
2023-03-06 18:23:54 -0500

    Added before and after photo and gallery to select photo

--- app/src/main/java/com/example/filtervision2/Filters.java
+++ app/src/main/java/com/example/filtervision2/Filters.java
@@ -1,18 +1,23 @@
 package com.example.filtervision2;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
 import android.graphics.PorterDuff;
 import android.widget.RadioButton;
 import android.widget.SeekBar;
+import android.widget.Button;
+import android.content.Intent;
 
 import androidx.appcompat.widget.SwitchCompat;
 import androidx.appcompat.widget.AppCompatSeekBar;
 public class Filters extends AppCompatActivity {
+    private static final int RESULT_LOAD_IMAGE = 1;
     RadioButton def;
     RadioButton red;
     RadioButton green;
@@ -22,6 +27,9 @@ public class Filters extends AppCompatActivity {
     SeekBar green_slider;
     SeekBar blue_slider;
     RadioButton select_image;
+    private final int GALLERY_REQ_CODE = 1;
+    ImageView imgAfter;
+    ImageView imgBefore;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,8 +45,21 @@ public class Filters extends AppCompatActivity {
         green_slider = findViewById(R.id.green_slider);
         blue_slider = findViewById(R.id.blue_slider);
 
-        select_image = findViewById(R.id.select_image2);
-        select_image.setChecked(true);
+//        select_image = findViewById(R.id.select_image2);
+//        select_image.setChecked(true);
+
+        imgAfter = findViewById(R.id.img_after);
+        imgBefore = findViewById(R.id.img_before);
+        Button btnGallery = findViewById(R.id.btn_gallery);
+
+        btnGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent iGallery = new Intent(Intent.ACTION_PICK);
+                iGallery.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(iGallery, GALLERY_REQ_CODE);
+            }
+        });
         red_slider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             int progressChangedValue = 0;
 
@@ -114,35 +135,51 @@ public class Filters extends AppCompatActivity {
     }
 
     public void applyFilter(View view) {
-        ImageView mountains = findViewById(R.id.image);
+        ImageView img = findViewById(R.id.img_after);
         if (def.isChecked()) {
-            mountains.getDrawable().clearColorFilter();
+            img.getDrawable().clearColorFilter();
         } else if (red.isChecked()) {
-            mountains.getDrawable().setColorFilter(0xffff7276, PorterDuff.Mode.MULTIPLY);
+            img.getDrawable().setColorFilter(0xffff7276, PorterDuff.Mode.MULTIPLY);
         } else if (green.isChecked()) {
-            mountains.getDrawable().setColorFilter(0xFF018786, PorterDuff.Mode.MULTIPLY);
+            img.getDrawable().setColorFilter(0xFF018786, PorterDuff.Mode.MULTIPLY);
         } else if (blue.isChecked()) {
-            mountains.getDrawable().setColorFilter(0xFF3700B3, PorterDuff.Mode.MULTIPLY);
+            img.getDrawable().setColorFilter(0xFF3700B3, PorterDuff.Mode.MULTIPLY);
         } else if (red_slider.getProgress() == 0 && green_slider.getProgress() == 0 && blue_slider.getProgress() == 0) {
-            mountains.getDrawable().clearColorFilter();
+            img.getDrawable().clearColorFilter();
         }  else if (custom.isChecked()) {
-            mountains.getDrawable().setColorFilter(Color.rgb(red_slider.getProgress(), green_slider.getProgress(), blue_slider.getProgress()), PorterDuff.Mode.MULTIPLY);
+            img.getDrawable().setColorFilter(Color.rgb(red_slider.getProgress(), green_slider.getProgress(), blue_slider.getProgress()), PorterDuff.Mode.MULTIPLY);
         } else {
-            mountains.getDrawable().clearColorFilter();
+            img.getDrawable().clearColorFilter();
         }
     }
 
-    public void applyImage(View view) {
-        RadioButton im1 = findViewById(R.id.select_image1);
-        RadioButton im2 = findViewById(R.id.select_image2);
-        ImageView image = findViewById(R.id.image);
-        if (im1.isChecked()) {
-            image.setImageResource(R.drawable.palm_tree);
-            applyFilter(image);
-        } else if (im2.isChecked()) {
-            image.setImageResource(R.drawable.mountains);
-            applyFilter(image);
-        }
-    }
+//    public void applyImage(View view) {
+//        RadioButton im1 = findViewById(R.id.select_image1);
+//        RadioButton im2 = findViewById(R.id.select_image2);
+//        ImageView image = findViewById(R.id.image);
+//        if (im1.isChecked()) {
+//            image.setImageResource(R.drawable.palm_tree);
+//            applyFilter(image);
+//        } else if (im2.isChecked()) {
+//            image.setImageResource(R.drawable.mountains);
+//            applyFilter(image);
+//        }
+//    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+
+            if (requestCode == GALLERY_REQ_CODE) {
+
+                imgAfter.setImageURI(data.getData());
+                applyFilter(imgAfter);
+                imgBefore.setImageURI(data.getData());
+                imgBefore.clearColorFilter();
+            }
+        }
+
+    }
 }
