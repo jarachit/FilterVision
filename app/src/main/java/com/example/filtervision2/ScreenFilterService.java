811ed42daa068693a682aa701dfc11ea78663625
jrachit <jrachit@umich.edu>
2023-03-29 11:17:08 -0400

    IME and back button now working typically.

--- app/src/main/java/com/example/filtervision2/ScreenFilterService.java
+++ app/src/main/java/com/example/filtervision2/ScreenFilterService.java
@@ -54,7 +54,7 @@ public class ScreenFilterService extends Service {
                 WindowManager.LayoutParams.MATCH_PARENT,
                 WindowManager.LayoutParams.MATCH_PARENT,
                 WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
-                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
+                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN,
                 PixelFormat.TRANSLUCENT
         );
         WindowManager windowManager = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
