61c7ce5b637ca5936fe47e94030e4604c76be57c
khhan <khhan@umich.edui>
2023-03-28 22:45:47 -0400

    Added Tutorial with 3 different tabs for basic explanation. Updated message in unimplemented tabs. Adjusted filter UI a bit.

--- app/src/main/java/com/example/filtervision2/MainActivity.java
+++ app/src/main/java/com/example/filtervision2/MainActivity.java
@@ -38,7 +38,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.test: i = new Intent(this, Test.class); startActivity(i); break ;
             case R.id.tutorial: i = new Intent(this, Tutorial.class); startActivity(i); break ;
             case R.id.presets: i = new Intent(this, Test.class); startActivity(i); break ;
-            case R.id.settings: i = new Intent(this, Tutorial.class); startActivity(i); break ;
+            case R.id.settings: i = new Intent(this, Test.class); startActivity(i); break ;
             default:break ;
         }
     }
