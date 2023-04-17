2ee5f934de8781a498e1f5458696863c35dd0a4f
jrachit <jrachit@umich.edu>
2023-04-17 09:33:05 -0400

    Preset Page UI improvements.

--- app/src/main/java/com/example/filtervision2/Filters.java
+++ app/src/main/java/com/example/filtervision2/Filters.java
@@ -543,12 +543,20 @@ public class Filters extends AppCompatActivity {
         startActivity(intent);
         String num = String.valueOf(Presets.presetList.size() + 1);
         String str = "Saved Matrix #" + num + "\n\n";
-        for (int j = 0; j < 3; j++) {
-            for(int i= (j * 3);i< (j*3) + 3;i++){
-                str = str + "     " + String.valueOf((int)currentFilter[i]);
-            }
-            str = str + "\n";
+//        for (int j = 0; j < 3; j++) {
+        for(int i= 0; i < 3; i++){
+            str = str + "     " + String.valueOf((int)currentFilter[i]);
         }
+        str = str + "\n";
+        for(int i= 5; i < 8; i++){
+            str = str + "     " + String.valueOf((int)currentFilter[i]);
+        }
+        str = str + "\n";
+        for(int i= 10; i < 13; i++){
+            str = str + "     " + String.valueOf((int)currentFilter[i]);
+        }
+        str = str + "\n";
+//        }
         str = str + "A:  " + alphaVal;
 //        editor.putString("FLOAT_ARR",str);
 //        editor.commit();
